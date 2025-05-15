10bfc32d69b04354829e5f1160ec9a183353cc3d
BrianSagra03 <gotenks1080@gmail.com>
2025-05-15 12:44:16 -0500

    DATABASE

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +78,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default="postgresql://postgres:ZDycOTdUHhaGqlPbzqCPLAbcQKbyJJla@postgres.railway.internal:5432/railway")
 }
 
 
